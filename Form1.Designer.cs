830ce757f2da7769426962933a224a7e0cd4805b
hamzattk <tutakhamza@gmail.com>
2024-12-18 10:40:17 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace donemprojeodevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(427, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "JPG",
+            "JPEG",
+            "PNG",
+            "BMP",
+            "GIF",
+            "TIFF"});
+            this.comboBox1.Location = new System.Drawing.Point(427, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 32);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "İmage Converter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(456, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Format";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(441, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Click the Button";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(699, 596);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace donemprojeodevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+
+  // OpenFileDialog nesnesi oluştur
+    OpenFileDialog openFileDialog = new OpenFileDialog();
+
+    // Yalnızca resim dosyalarını göster
+    openFileDialog.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+5
+    // Kullanıcının bir dosya seçmesini bekle
+    if (openFileDialog.ShowDialog() == DialogResult.OK)
+    {
+        // Seçilen dosyayı PictureBox'a yükle
+        pictureBox1.ImageLocation = openFileDialog.FileName;
+    }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen önce bir resim seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // OpenFileDialog ile resim kaydetme yeri sor
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // ComboBox'tan seçilen formatı al
+            string selectedFormat = comboBox1.SelectedItem.ToString();
+
+            // Dosya filtrelerini ayarla
+            switch (selectedFormat)
+            {
+                case "JPG":
+                case "JPEG":
+                    saveFileDialog.Filter = "JPEG Dosyaları|*.jpg;*.jpeg";
+                    break;
+                case "PNG":
+                    saveFileDialog.Filter = "PNG Dosyaları|*.png";
+                    break;
+                case "BMP":
+                    saveFileDialog.Filter = "BMP Dosyaları|*.bmp";
+                    break;
+                case "GIF":
+                    saveFileDialog.Filter = "GIF Dosyaları|*.gif";
+                    break;
+                case "TIFF":
+                    saveFileDialog.Filter = "TIFF Dosyaları|*.tiff";
+                    break;
+                default:
+                    MessageBox.Show("Geçersiz format seçildi.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+            }
+
+            // Kullanıcı kaydetme yeri seçerse
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.Drawing.Imaging.ImageFormat format = null;
+
+                // Seçilen formata uygun ImageFormat belirle
+                switch (selectedFormat)
+                {
+                    case "JPG":
+                    case "JPEG":
+                        format = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        break;
+                    case "PNG":
+                        format = System.Drawing.Imaging.ImageFormat.Png;
+                        break;
+                    case "BMP":
+                        format = System.Drawing.Imaging.ImageFormat.Bmp;
+                        break;
+                    case "GIF":
+                        format = System.Drawing.Imaging.ImageFormat.Gif;
+                        break;
+                    case "TIFF":
+                        format = System.Drawing.Imaging.ImageFormat.Tiff;
+                        break;
+                }
+
+                // Resmi seçilen formata dönüştürüp kaydet
+                pictureBox1.Image.Save(saveFileDialog.FileName, format);
+                MessageBox.Show("Resim başarıyla kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ComboBox'a formatları ekle
+            comboBox1.Items.Add("JPG");
+            comboBox1.Items.Add("JPEG");
+            comboBox1.Items.Add("PNG");
+            comboBox1.Items.Add("BMP");
+            comboBox1.Items.Add("GIF");
+            comboBox1.Items.Add("TIFF");
+            comboBox1.SelectedIndex = 0; // Va
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
